06c479a86778688886ae20162327ef74e1da3a00
nils&paul <info@nilsenpaul.nl>
2021-03-10 14:39:03 +0100

    Update resources

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 
 mix
-    .js('src/main.js', 'resources/easy-search.js')
+    .js('src/main.js', 'resources/easy-search.js').vue()
     .sass('src/main.scss', 'resources/easy-search.css')
     .options({
         processCssUrls: false
